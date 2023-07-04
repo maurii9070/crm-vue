47aa687848c9b6aae380047d402409665e9a4687
Mauricio Nehuen Velazquez <mauri_i9070@hotmail.com>
2023-07-04 20:37:20 -0300

    agregando Links para navegar por diferentes paginas, se agrego estilos generales para los links

--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -1,11 +1,11 @@
 <script setup>
-	import { RouterLink } from 'vue-router';
+	import RouterLink from '../components/ui/RouterLink.vue';
 </script>
 
 <template>
 	<div>
-		<div class="">
-			<RouterLink :to="{ name: 'inicio' }">Volver</RouterLink>
+		<div class="flex justify-end">
+			<RouterLink to="inicio">Volver</RouterLink>
 		</div>
 
 		<h1 class="text-4xl font-extrabold text-slate-500">Agregar Cliente</h1>
